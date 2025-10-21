14a34efb022b8e5901c9c52175c5837fa158c8f8
Bipentium25 <bipentium25@gmail.com>
2025-10-21 12:42:26 -0700

    Update swap_meet/item.py
    Co-authored-by: Mikelle <119449245+mikellewade@users.noreply.github.com>

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,10 +2,7 @@ import uuid
 
 class Item:
     def __init__(self, id=None, condition=0.0, age=0):
-        if id is None:
-            self.id = uuid.uuid4().int
-        else:
-            self.id = id
+        self.id = id if id is not None else uuid.uuid4().int
         self.condition = condition
         self.age = age
     
